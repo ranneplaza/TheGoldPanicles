1a8c1fa66f1a606f22d07611f507156f89c659e7
ranne <ranne.plaza@carsu.edu.com>
2025-04-24 22:58:52 +0800

    icons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import './assets/main.css' // your custom styles should come after vuetify
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-// Vuetify
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -13,16 +11,15 @@ import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-
 const vuetify = createVuetify({
   icons: {
-    defaultSet: 'mdi', // This is already the default value - only for display purposes
+    defaultSet: 'mdi',
   },
   components,
   directives,
 })
 
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
